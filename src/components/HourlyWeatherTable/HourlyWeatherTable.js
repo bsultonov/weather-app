5f5a04d4a9a470c2bc46d4653a684b74224e6c52
Bositkhon <s.bositkhon@gmail.com>
2021-06-14 23:18:44 +0500

    fixed locales for toLocaleTimeString()

--- src/components/HourlyWeatherTable/HourlyWeatherTable.js
+++ src/components/HourlyWeatherTable/HourlyWeatherTable.js
@@ -16,7 +16,7 @@ const HourlyWeatherTable = ({hourlyWeather}) => {
                 <tbody>
                     {hourlyWeather.map(hour => (
                         <tr key={hour.dt}>
-                            <td>{unixToDate(hour.dt).toLocaleTimeString("en_US", {hour: '2-digit', minute: '2-digit'})}</td>
+                            <td>{unixToDate(hour.dt).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</td>
                             <td>{parseInt(hour.temp - 273.15)}&deg;C</td>
                             <td>{hour.weather[0].main}</td>
                             <td><img src={`http://openweathermap.org/img/w/${hour.weather[0].icon}.png`} alt={hour.weather[0].main} /></td>
